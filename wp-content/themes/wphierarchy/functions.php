95957066d2d1c9dddf6c1350dda07151db5c29eb
Marco Valeri <marcovaleri@hotmail.it>
2021-12-17 09:35:49 +0000

    Add js files

--- wp-content/themes/wphierarchy/functions.php
+++ wp-content/themes/wphierarchy/functions.php
@@ -25,11 +25,20 @@ add_theme_support('starter-content');
 // Load in our CSS
 function wphierarchy_enqueue_styles() {
 
+    wp_enqueue_style('varela-font-css', 'https://fonts.googleapis.com/css?family=Varela+Round', [], '', 'all');
     wp_enqueue_style('main-css', get_stylesheet_directory_uri() . '/style.css', [], time(), 'all');
+    wp_enqueue_style('custom-css', get_stylesheet_directory_uri() . '/assets/css/custom.css', ['main-css'], time(), 'all');
 
 }
-
 add_action('wp_enqueue_scripts', 'wphierarchy_enqueue_styles');
+
+// Load in out JS
+function wphierarchy_enqueue_scripts() {
+
+    wp_enqueue_script('jquery-theme-js', get_stylesheet_directory_uri() . '/assets/js/jquery.theme.js', ['jquery'], time(), true);
+
+}
+add_action('wp_enqueue_scripts', 'wphierarchy_enqueue_scripts');
 
 // Register Menu Locations
 register_nav_menus([
