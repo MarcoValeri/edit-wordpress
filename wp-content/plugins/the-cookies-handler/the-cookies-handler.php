7f5bd0e945378719b5daebf1e184e67fe700803b
Marco Valeri <marcovaleri@hotmail.it>
2022-03-14 07:24:33 +0000

    Add sub pages

--- wp-content/plugins/the-cookies-handler/the-cookies-handler.php
+++ wp-content/plugins/the-cookies-handler/the-cookies-handler.php
@@ -29,6 +29,24 @@ function the_cookie_handler_page() {
         'deshicons-wordpress-alt',
         100
     );
+
+    add_submenu_page(
+        'the-cookie-handler-page',
+        'Menu 1',
+        'Feature 1',
+        'manage_options',
+        'the-cookie-handler-page-menu-1',
+        'the_cookie_handler_page_markup'
+    );
+
+    add_submenu_page(
+        'the-cookie-handler-page',
+        'Menu 2',
+        'Feature 2',
+        'manage_options',
+        'the-cookie-handler-page-menu-2',
+        'the_cookie_handler_page_markup'
+    );
 }
 add_action('admin_menu', 'the_cookie_handler_page');
 
@@ -45,5 +63,41 @@ function the_cookie_handler_page_markup() {
     </div>
     <?php
 }
+
+function the_cookie_handler_subpage_markup() {
+
+    // Double checj user capabilities
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+    ?>
+    <div class="wrap">
+        <h1><?php esc_html_e(get_admin_page_title()); ?></h1>
+        <p><?php esc_html_e('Some content.', 'the-cookie-handler-page'); ?></p>
+    </div>
+    <?php
+}
+
+function the_cookie_handler_default_sub_pages() {
+    // Can add page as a submenu using the following:
+    // add_dashboard_page()
+    // add_posts_page()
+    // add_media_page()
+    // add_pages_page()
+    // add_comments_page()
+    // add_theme_page()
+    // add_plugins_page()
+    // add_management_page()
+    // add_options_page()
+
+    add_theme_page(
+        __('Cool Default Sub Page', 'the-cookie-handler'),
+        __('Custom Sub Page', 'the-cookie-handler'),
+        'manage_options',
+        'the-cookie-handler-subpage',
+        'the_cookie_handler_page_markup'
+    );
+}
+add_action('admin_menu', 'the_cookie_handler_default_sub_pages');
 
 ?>
